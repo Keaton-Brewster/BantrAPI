e28f128caeec0e295491653d80a2898cca431da9
Keaton-Brewster <keaton.brewster@gmail.com>
2021-07-20 07:43:57 -0500

    Just trying to get db connection and querying working

---
+++ BatnrAPI/Models/UncommonFields.cs
@@ -0,0 +1,40 @@
+using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace BantrAPI.Models.UncommonFields
+{
+    [BsonIgnoreExtraElements]
+    public class Messages
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string sender_id { get; set; }
+
+        [BsonElement("content")]
+        public string Content { get; set; }
+
+        [BsonElement("senderName")]
+        public string SenderName { get; set; }
+    }
+
+    // public interface IMessages
+    // {
+    //     string sender_id { get; set; }
+    //     string Content { get; set; }
+    //     string SenderName { get; set; }
+    // }
+
+    public class Members
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string member_id { get; set; }
+    }
+
+
+    // public interface IMembers
+    // {
+    //     string member_id { get; set; }
+    // }
+}
